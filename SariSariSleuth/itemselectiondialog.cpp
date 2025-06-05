c7e1db4e52e7916b56860217d819fe7b7059611a
Norvzki <norvel.ian.gendoy@gmail.com>
2025-06-06 02:20:59 +0800

    "Changes to new window popping (add, manual add)"

--- SariSariSleuth/itemselectiondialog.cpp
+++ SariSariSleuth/itemselectiondialog.cpp
@@ -4,7 +4,7 @@
 
 ItemSelectionDialog::ItemSelectionDialog(StockModel *model, QWidget *parent) : QDialog(parent) , stockModel(model) {
     setWindowTitle("Select Item");
-    setMinimumWidth(400);
+    setMinimumWidth(700);
 
     // Create table view
     tableView = new QTableView(this);
@@ -14,11 +14,72 @@ ItemSelectionDialog::ItemSelectionDialog(StockModel *model, QWidget *parent) : Q
     tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     tableView->verticalHeader()->setVisible(false);
 
+    // Apply your custom stylesheet
+    tableView->setStyleSheet(
+        "QTableView {"
+        "    color: black;"
+        "    background-color: rgb(255, 255, 255);"
+        "    selection-background-color: rgb(230, 240, 255);"
+        "    selection-color: rgb(0, 71, 255);"
+        "    font: 700 9pt \"Montserrat\";"
+        "    border: 1px solid rgb(220, 220, 220);"
+        "    border-bottom-left-radius: 10px;"
+        "    border-bottom-right-radius: 10px;"
+        "    gridline-color: rgb(220, 220, 220);"
+        "}"
+        "QHeaderView::section {"
+        //"    margin-top: 2px;  "
+        "    color:black;"
+        "    background-color: rgb(245, 245, 245);"
+        "    font: 700 9pt \"Montserrat\";"
+        "    padding: 4px;"
+        "    border: 1px solid rgb(200, 200, 200);"
+        "}"
+        );
+
     // Create quantity spinbox
     quantitySpinBox = new QSpinBox(this);
     quantitySpinBox->setMinimum(1);
     quantitySpinBox->setMaximum(9999);
     quantitySpinBox->setValue(1);
+    quantitySpinBox->setStyleSheet(
+        "QSpinBox {"
+        "    color: rgb(0, 71, 255);"
+        "    background-color: rgb(255, 255, 255);"
+        "    selection-background-color: rgb(230, 240, 255);"
+        "    selection-color: rgb(0, 71, 255);"
+        "    font: 700 9pt \"Montserrat\";"
+        "    padding-right: 20px;"  // Reserve space for the buttons
+        "    border: 1px solid rgb(200, 200, 200);"
+        "    border-radius: 4px;"
+        "}"
+
+        "QSpinBox::up-button {"
+        "    subcontrol-origin: border;"
+        "    subcontrol-position: top right;"
+        "    width: 16px;"
+        "    height: 10px;"
+        "    background-color: rgb(240, 240, 240);"
+        "    border-left: 1px solid rgb(200, 200, 200);"
+        "    border-bottom: 1px solid rgb(200, 200, 200);"
+        "}"
+
+        "QSpinBox::down-button {"
+        "    subcontrol-origin: border;"
+        "    subcontrol-position: bottom right;"
+        "    width: 16px;"
+        "    height: 10px;"
+        "    background-color: rgb(240, 240, 240);"
+        "    border-left: 1px solid rgb(200, 200, 200);"
+        "    border-top: 1px solid rgb(200, 200, 200);"
+        "}"
+
+        "QSpinBox::up-arrow, QSpinBox::down-arrow {"
+        "    width: 7px;"
+        "    height: 7px;"
+        "}"
+        );
+
 
     // Create buttons
     acceptButton = new QPushButton("Accept", this);
