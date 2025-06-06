21e02696dfeed4fff390140d2d697e947b74181f
0Msen <dosdosjohnsen@gmail.com>
2025-06-06 16:43:49 +0800

    added icon, changed some things

--- SariSariSleuth/main.cpp
+++ SariSariSleuth/main.cpp
@@ -3,10 +3,12 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QIcon>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv); // Our main application
-    
+
+    a.setWindowIcon(QIcon(":/resources/favicon.ico"));
     QTranslator translator; // Translation services
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
